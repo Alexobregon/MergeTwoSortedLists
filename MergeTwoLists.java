db7b1e17bc1ed8aa8b1236af4233f54fec1192c3
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-21 01:15:36 -0800

    added prehead and prev Node

--- MergeTwoLists.java
+++ MergeTwoLists.java
@@ -1,6 +1,8 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
       
+        ListNode prehead = new ListNode(-1);
+        ListNode prev = prehead;
 
         return head;
     }
