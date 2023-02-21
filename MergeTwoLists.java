1559824c06123ca9f345b5d32961b9086f7c7390
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-21 01:16:38 -0800

    added while loop with logic and traversal

--- MergeTwoLists.java
+++ MergeTwoLists.java
@@ -4,6 +4,18 @@ class Solution {
         ListNode prehead = new ListNode(-1);
         ListNode prev = prehead;
 
+        while(list1 != null && list2 != null){
+           
+            if(list1.val <= list2.val){
+                prev.next = list1;
+                list1 = list1.next;
+            } else {
+                prev.next = list2;
+                list2 = list2.next;
+            }
+            prev = prev.next;
+        }
+
         return head;
     }
 }
